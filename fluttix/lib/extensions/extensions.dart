3d349c1e19fe347796fde4543d3ced302cb07210
ivan <alvianbuyar@gmail.com>
2021-07-19 14:36:15 +0700

    first commit

---
+++ fluttix/lib/extensions/extensions.dart
@@ -0,0 +1,7 @@
+import 'package:fluttix/models/models.dart';
+import 'package:fluttix/services/services.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+
+part 'firebase_user_extension.dart';
+part 'date_time_extension.dart';
+part 'string_extension.dart';
